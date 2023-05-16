2f79da6ea66d518d0a033d9a43ffef7411fe4145
Dodistywn <dodisetya000@gmail.com>
2023-05-16 21:14:44 +0700

    add script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function countMe(arr) {
+    let num = {};
+    for (let i = 0; i < arr.length; i++) {
+        let obj = arr[i];
+        if (num [obj]) {
+            num [obj] ++;
+        } else {
+            num [obj] = 1;
+        }
+    }
+    return num;
+  }
+  
+  console.log(countMe(['Sofyan', 'Ricky', 'Sofyan', 'Semmi', 'Semmi', 'Wika']));
+  // { Sofyan: 2, Ricky: 1, Semmi: 2, Wika: 1 }
+  
+  console.log(countMe([ 1, 15, 9, 10, 8, 1, 12, 15, 10, 3 ]));
+  // { '1': 2, '3': 1, '8': 1, '9': 1, '10': 2, '12': 1, '15': 2 }
