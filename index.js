b665be25ea9f78fc56f86b2cad32a57822f66804
Delin Herliana Pashs <delinherlianapasha@gmail.com>
2023-05-16 20:51:45 +0700

    add countME

---
+++ index.js
@@ -0,0 +1,18 @@
+function countMe(arr) {
+  let countObj = {};
+
+  for (let value of arr) {
+    if (countObj[value]) {
+      countObj[value] += 1;
+    } else {
+      countObj[value] = 1;
+    }
+  }
+
+  return countObj;
+}
+
+console.log(countMe(["Sofyan", "Ricky", "Sofyan", "Semmi", "Semmi", "Wika"]));
+// { Sofyan: 2, Ricky: 1, Semmi: 2, Wika: 1 }
+console.log(countMe([1, 15, 9, 10, 8, 1, 12, 15, 10, 3]));
+// { '1': 2, '3': 1, '8': 1, '9': 1, '10': 2, '12': 1, '15': 2 }
