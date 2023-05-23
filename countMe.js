653cdfc78cba9859cf748c5b4229e76ea3219e94
GhafickyRenku <mghaficky.r@gmail.com>
2023-05-23 10:44:51 +0700

    kumpul tugas count me

--- countMe.js
+++ countMe.js
@@ -14,3 +14,10 @@ function countMe(arr) {
     return countObj;
   }
 
+  console.log(countMe(['Sofyan', 'Ricky', 'Sofyan', 'Semmi', 'Semmi', 'Wika']));
+  // Output: { Sofyan: 2, Ricky: 1, Semmi: 2, Wika: 1 }
+  
+  console.log(countMe([1, 15, 9, 10, 8, 1, 12, 15, 10, 3]));
+  // Output: { '1': 2, '3': 1, '8': 1, '9': 1, '10': 2, '12': 1, '15': 2 }
+
+  
